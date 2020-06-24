c9f9e6abdd3d238a93461382d103ecdb5259b824
Brett Lee <spareafro@post.com>
2020-06-24 23:34:31 +0100

    Completed First Section

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 let header = document.getElementById("header");
+let marker = document.querySelector('#marker');
+let item = document.querySelectorAll('nav a');
 
 function toggle() {
 	header.classList.toggle('active');
 }
 
-function toggleHome() {
-	if (header.classList.contains('active')) {
-		header.classList.remove('active');
-	}
-	else {
-		return;
-	}
+function indicator(e) {
+	marker.style.left = e.offsetLeft+"px";
+	marker.style.width = e.offsetWidth+"px";
 }
+
+item.forEach(link => {
+	link.addEventListener('click', (e) => {
+		indicator(e.target);
+	});
+});
+
+marker.style.left = '0px';
+marker.style.width = '97px';
